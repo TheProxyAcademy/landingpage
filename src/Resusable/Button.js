0d4a9eb579278cb0463483fb2921c2e6a589619f
nuelKaiser <majekodunmiimmanuel9@gmail.com>
2022-09-29 15:16:15 +0100

    added googlr form external link

--- src/Resusable/Button.js
+++ src/Resusable/Button.js
@@ -3,8 +3,10 @@ import './Button.css';
 
 const Button = ({words, white,}) => {
   return (
-    <button className={white ? 'white' : 'button'} >
-        {words}
+    <button className={white ? 'white' : 'button'}  >
+        <a href='https://docs.google.com/forms/d/e/1FAIpQLSceBtAQ4Yt5BtjgK89Vk07SS_nhWs56oGTQvv4raTFMnuZaJg/viewform'>
+          {words}
+        </a>
     </button>
   )
 }
