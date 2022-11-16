51ac05f8a21128059a1eede894064a990d1e0165
nuelKaiser <majekodunmiimmanuel9@gmail.com>
2022-11-16 22:17:30 +0100

    new link for registration added

--- src/Resusable/Button.js
+++ src/Resusable/Button.js
@@ -3,7 +3,7 @@ import './Button.css';
 
 const Button = ({words, white,}) => {
   return (
-        <a className={white ? 'white' : 'button'} href='https://docs.google.com/forms/d/e/1FAIpQLSceBtAQ4Yt5BtjgK89Vk07SS_nhWs56oGTQvv4raTFMnuZaJg/viewform' target='_blank' rel='noreferrer'>
+        <a className={white ? 'white' : 'button'} href='https://tinyurl.com/theproxyacademy' target='_blank' rel='noreferrer'>
           {words}
         </a>
   )
